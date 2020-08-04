80c4b784488b35349a786786ff4240e5af9dbc81
mplesser <mlesser@arizona.edu>
2020-08-04 09:02:22 -0700

    cleanup

--- start_monitor.bat
+++ start_monitor.bat
@@ -2,4 +2,4 @@
 
 rem An example...
 
-python C:\azcam\azcammonitor\azcammonitor.py -configfile "/azcam/azcam-vatt/bin/parameters_vatt_monitor.ini"
+start/min python C:\azcam\azcammonitor\azcammonitor.py -configfile "/azcam/azcam-vatt/bin/parameters_vatt_monitor.ini"
