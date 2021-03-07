5d4b9f8dd9c9563b19faab9ff15c09f87c1e9a73
Michael Lesser <mlesser@arizona.edu>
2021-03-07 07:50:27 -0700

    21.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="azcam-monitor",
-    version="21.1.2",
+    version="21.2.0",
     description="azcam processes monitor and control app",
     long_description=long_description,
     long_description_content_type="text/markdown",
